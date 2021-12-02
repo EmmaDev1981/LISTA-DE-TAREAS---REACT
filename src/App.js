f10e14c8cdd52ef944e71d36ff15e2495dbe321b
Emmanuel Navas <82782857+EmmaDev1981@users.noreply.github.com>
2021-12-02 19:15:57 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,10 @@ import './App.css'
 import TodoList from "./components/TodoList";
 
 function App() {
-  
   return (
     <div className="todo-app">
       <TodoList />
     </div>
   );
 }
-
 export default App;
